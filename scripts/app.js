58102d505f576a4bd7e64129df4c9e0a5e6ede40
Brett Haymaker <brett.haymaker@gmail.com>
2017-10-04 16:27:11 -0500

    adds sprite, successfully renders a 10 of diamonds using canvas and sprite positions, must refactor. Next steps - map out sprite locations in object for easy reference later

---
+++ scripts/app.js
@@ -0,0 +1,44 @@
+(function(){
+  var canvas = document.querySelector('.card-canvas');
+
+  var ctx = canvas.getContext('2d');
+
+  var img = new Image();
+
+  img.src = '../assets/card-sprite.png';
+
+  console.log(img);
+
+  // Diamonds
+  // Big Diamond on card
+  ctx.drawImage(img, 160, 249, 138, 138, 5, 58, 30, 88);
+
+
+  var img = new Image();
+
+  img.src = '../assets/card-sprite.png';
+
+  console.log(img);
+
+  // Diamonds
+  // Number on Card
+  ctx.drawImage(img, 820,239, 55, 51, 3, 8, 12, 32);
+
+  var img = new Image();
+
+  img.src = '../assets/card-sprite.png';
+
+  console.log(img);
+
+  // Diamonds
+  // small diamond on Card
+  ctx.drawImage(img, 711, 319, 53, 49, 25, 8, 12, 32);
+
+})();
+
+
+// Next steps:
+
+// Map out Sprite locations
+// in an object / dictionary
+// for easy reference in rendering functions later on
